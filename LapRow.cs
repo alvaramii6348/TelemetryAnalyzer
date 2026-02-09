936546e25f043fb4d3eafe5e61573f41033b4fcd
ialvara1 <ialvara1@charlotte.edu>
2026-02-09 18:49:15 -0500

    Initial project setup and cleanup

---
+++ LapRow.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1;
+
+public class LapRow
+{
+    public string? CarNumber {get; set;}
+    public string? LastName {get; set;}
+    public string? Sector {get; set;}
+    public double Time {get; set;} 
+    public double EntryTime {get; set;}
+    public double ExitTime {get; set;}
+    public int Lap {get; set;}
+    public string? Flag {get; set;}
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+
+string[] file = File.ReadAllLines("Chevrolet Detroit Grand Prix_Practice 2.csv");
+
+Console.WriteLine("File headers: " + file[0]);
+
+List<Class> rows = new List<Class>();
+
+
+
+
+string[] col;
+int length = file.Length-1;
+
+for(int i = 1; i < file.Length; i++)
+{
+    col = file[i].Split(',');
+
+    Class row = new Class
+    {
+        CarNumber = col[0],
+        Sector = col[2],
+        Time = double.Parse(col[3])
+    };
+
+    rows.Add(row);
+
+    Console.WriteLine(row.CarNumber);
+    // if(col[])
+    // if (col[3].Equals("LAPS"))
+    // {
+    //     continue;
+    // }
+    // else
+    // {
+    //     Console.WriteLine(col[3]);
+    // }
+}
+
+Dictionary<string, List<Class>> cars = new Dictionary<string, List<Class>>;
+
+foreach(Class r in rows)
+{
+    
+}
